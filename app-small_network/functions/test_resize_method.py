7203007ed920eeb906b06b9798996b92c9facd1c
AlbertHansen <albert.berg@hotmail.com>
2024-03-27 09:11:44 +0100

    Data acquisition for dim reduction

---
+++ app-small_network/functions/test_resize_method.py
@@ -0,0 +1,112 @@
+# import packages
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+import time
+
+# Datasets
+train_paths = [
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_AREA/train',
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_BICUBIC/train',
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_BILINEAR/train',
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_GAUSSIAN/train',
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_LANCZOS3/train',
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_LANCZOS5/train',
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_MITCHELLCUBIC/train',
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_NEAREST_NEIGHBOR/train'   
+]
+test_paths = [
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_AREA/test',
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_BICUBIC/test',
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_BILINEAR/test',
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_GAUSSIAN/test',
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_LANCZOS3/test',
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_LANCZOS5/test',
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_MITCHELLCUBIC/test',
+    '/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16_NEAREST_NEIGHBOR/test'   
+]
+csv_names = [
+    '/home/ubuntu/approximate_computing_in_CNN/app-small_network/results/AREA.csv',
+    '/home/ubuntu/approximate_computing_in_CNN/app-small_network/results/BICUBIC.csv',
+    '/home/ubuntu/approximate_computing_in_CNN/app-small_network/results/BILINEAR.csv',
+    '/home/ubuntu/approximate_computing_in_CNN/app-small_network/results/GAUSSIAN.csv',
+    '/home/ubuntu/approximate_computing_in_CNN/app-small_network/results/LANCZOS3.csv',
+    '/home/ubuntu/approximate_computing_in_CNN/app-small_network/results/LANCZOS5.csv',
+    '/home/ubuntu/approximate_computing_in_CNN/app-small_network/results/MITCHELLCUBIC.csv',
+    '/home/ubuntu/approximate_computing_in_CNN/app-small_network/results/NEAREST_NEIGHBOR.csv'
+]
+
+#train = tf.data.Dataset.load('/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16/train')
+#test  = tf.data.Dataset.load('/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16/test')
+
+def preprocess(example):
+    image = example['image']
+    image.set_shape([16, 16, 1])
+    label = example['label']
+    label = tf.one_hot(label, depth=100)  # One-hot encode the labels
+    return image, label
+
+def format_set(train_set, test_set):
+    # format and cache
+    train_set_formatted = train_set.map(preprocess, num_parallel_calls=tf.data.AUTOTUNE)
+    train_set_formatted = train_set_formatted.cache()
+    train_set_formatted = train_set_formatted.batch(256)
+    train_set_formatted = train_set_formatted.prefetch(tf.data.AUTOTUNE)
+    test_set_formatted = test_set.map(preprocess, num_parallel_calls=tf.data.AUTOTUNE)
+    test_set_formatted = test_set_formatted.batch(256)
+    test_set_formatted = test_set_formatted.cache()
+    test_set_formatted = test_set_formatted.prefetch(tf.data.AUTOTUNE)
+    return train_set_formatted, test_set_formatted
+
+# Keeping time
+class TimeHistory(tf.keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.times = []
+
+    def on_epoch_begin(self, batch, logs={}):
+        self.epoch_time_start = time.time()
+
+    def on_epoch_end(self, batch, logs={}):
+        self.times.append(time.time() - self.epoch_time_start)
+
+time_callback = TimeHistory()
+
+# Model 
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(64, (2, 2), activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Conv2D(32, (2, 2), activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(100),
+    tf.keras.layers.Softmax()
+])
+
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.MeanSquaredError(),
+    metrics=['accuracy']
+)
+
+#train = tf.data.Dataset.load('/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16/train')
+#test  = tf.data.Dataset.load('/home/ubuntu/tensorflow_datasets/cifar100_grey_16x16/test')
+
+for i in range(len(csv_names)):
+    # fetch datasets
+    train = tf.data.Dataset.load(train_paths[i])
+    test  = tf.data.Dataset.load(test_paths[i])
+    train, test = format_set(train, test)
+
+    # Train
+    history = model.fit(train, epochs=250, validation_data=test, callbacks=[time_callback])
+
+    # Convert the history.history dict to a pandas DataFrame
+    hist_df = pd.DataFrame(history.history)
+
+    # Add epoch times
+    hist_df['time'] = time_callback.times
+
+    # Save to csv
+    hist_df.to_csv(csv_names[i])
