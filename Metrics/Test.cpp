894506b90fe986659e4db67de0e7087bdd4cc0e9
AlbertHansen <albert.berg@hotmail.com>
2024-05-02 14:42:54 +0200

    sync

--- Metrics/Test.cpp
+++ Metrics/Test.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <vector>
+
+std::vector<intmax_t> expected = {2};
+std::vector<intmax_t> actual = {4};
+
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    intmax_t xorResult = expected[0] ^ actual[0];
+    std::cout << xorResult;
+};
