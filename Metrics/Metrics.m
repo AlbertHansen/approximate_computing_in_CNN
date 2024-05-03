5d3b6068a2be45a5bf00301b5d3f74ed2d295501
Simon <sjepse19@student.aau.dk>
2024-05-03 13:38:27 +0200

    Matlab metrics

--- Metrics/Metrics.m
+++ Metrics/Metrics.m
@@ -1,10 +1,20 @@
-actual = readmatrix("expectedFile.csv");
-actual = actual(1:end-1);
-expected = readmatrix("actualFile.csv");
+actual = readmatrix("Actual.csv");
+
+expected = readmatrix("Expected.csv");
 
 diff = expected-actual;
+binEdges = linspace(-9.5,9.5,40);
 
-WCE = max(diff)
+f = figure('Position',[10 10 600 300]);
+hold on;
+grid;
+handle = histogram(diff,binEdges, "Normalization","probability");
+xlabel("$$\textnormal{Error}\ [\cdot]$$", Interpreter="latex");
+ylabel("$$\textnormal{Probability}\ [\cdot]$$", Interpreter="latex");
+ylim([0 1]);
+xlim([-2 2]);
+hold off; 
 
+exportgraphics(f,"histogram.pdf","Resolution",150);
 
-histogram(diff,50);
+%histogram(diff,5, "Normalization","probability")
