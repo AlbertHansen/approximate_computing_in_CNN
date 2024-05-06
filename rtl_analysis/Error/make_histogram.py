ae3da5987443ad337011b127ccd8c0cf65f95e35
Simon <sjepse19@student.aau.dk>
2024-05-06 13:41:38 +0200

    Albot

--- rtl_analysis/Error/make_histogram.py
+++ rtl_analysis/Error/make_histogram.py
@@ -28,8 +28,8 @@ plt.rc('xtick',     labelsize   = SMALL_SIZE)   # fontsize of the tick labels
 plt.rc('ytick',     labelsize   = SMALL_SIZE)   # fontsize of the tick labels
 plt.rc('legend',    fontsize    = SMALL_SIZE)   # legend fontsize
 plt.rc('figure',    titlesize   = BIGGER_SIZE)  # fontsize of the figure title
-# plt.rc('text',      usetex      = True)         # use latex for interpreter
-# plt.rc('font',      family      = 'Computer Modern Serif')      # use serif font (to look like latex)
+plt.rc('text',      usetex      = True)         # use latex for interpreter
+plt.rc('font',      family      = 'Computer Modern Serif')      # use serif font (to look like latex)
 plt.rc('font',      weight      = 'heavy')     # controls font weight
 
 cm = 1/2.54                                     # centimeters in inches
@@ -44,7 +44,7 @@ expected = np.loadtxt("./Error/Error_files/Expected.csv", delimiter=",")
 diff = expected - actual
 
 # Define bin edges for histogram
-bin_edges = np.linspace(-9.5, 9.5, 40)
+bin_edges = np.linspace(-19.5, 19.5, 80)
 
 # Create the histogram
 fig = plt.figure(layout='constrained', figsize=(17.01*cm, 8*cm))
@@ -52,7 +52,7 @@ plt.hist(diff, bins=bin_edges, density=True)
 plt.xlabel("Error [.]")
 plt.ylabel("Probability [.]")
 plt.grid(True)
-plt.xlim(-2, 2)
+# plt.xlim(-2, 2)
 plt.ylim(0, 1)
 
 # Save the plot as PDF
