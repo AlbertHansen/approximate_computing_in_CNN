08bb8caebfe556086ef819aa709829f9ddc8ad72
AlbertHansen <albert.berg@hotmail.com>
2024-05-22 10:58:47 +0200

    sync

--- convergence_of_stat_and_approx_model/mul8s_1kv9_stats_and_approx.py
+++ convergence_of_stat_and_approx_model/mul8s_1kv9_stats_and_approx.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import csv
 from NoisyLayers import * 
 from dataset_manipulation import *
-from test_custom_layers import * 
+# from test_custom_layers import * 
 from collections import defaultdict
 from tensorflow.keras import layers, models
 from my_csv import weights_to_csv, csv_to_weights
@@ -187,10 +187,10 @@ def main() -> None:
     model.build((None, 16, 16, 1))
     model.summary()
 
-    with open('mul8s_1kv9_stats_and_approx.csv', 'r') as file:
+    with open('mul8s_1kv9_stats_and_approx.csv', 'a') as file:
         writer = csv.writer(file)
 
-        for j in range(5):
+        for j in range(5, 10):
 
             csv_to_weights(model, f'1KV9_weights/save_{45 + j*5}')
 
